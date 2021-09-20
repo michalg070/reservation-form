2e247f977e39284f759fe9bb883db4edebc45531
michalg070 <diegonbth@gmail.com>
2021-09-20 19:38:13 +0200

    feat ✨: create ReservationForm component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app" class="main">
     <div class="main__wrapper">
-      test
+      <ReservationForm :rating="1.5" :price="232" :reviews="234" />
     </div>
   </div>
 </template>
 
 <script>
+  import ReservationForm from "./components/ReservationForm.vue";
   export default {
     name: "App",
-    components: {},
+    components: {
+      ReservationForm,
+    },
   };
 </script>
 
---
+++ src/components/ReservationForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form class="reservation-form">
+    <div class="reservation-form__wrapper">
+      <header class="reservation-form__header">
+        <h5 class="reservation-form__header__price">{{ price }} zł</h5>
+
+        <div class="reservation-form__header__reviews">
+          {{ rating }}
+
+          <span class="reservation-form__header__count">{{ reviews }}</span>
+        </div>
+      </header>
+    </div>
+  </form>
+</template>
+
+<script>
+  export default {
+    props: {
+      price: {
+        type: Number,
+      },
+      rating: {
+        type: Number,
+      },
+      reviews: {
+        type: Number,
+      },
+    },
+
+    components: {},
+  };
+</script>
