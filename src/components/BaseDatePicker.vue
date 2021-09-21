5bed7e5eaa9c62124329d01674c9fab3d4bcc143
michalg070 <diegonbth@gmail.com>
2021-09-21 23:57:24 +0200

    wip 🚧: working on Calendar component

--- src/components/BaseDatePicker.vue
+++ src/components/BaseDatePicker.vue
@@ -6,6 +6,7 @@
       <input
         :id="inputNames.CHECK_IN"
         :class="{'base-date-picker__trigger--active': activeInput === inputNames.CHECK_IN}"
+        @click="showCalendar(inputNames.CHECK_IN)"
         class="base-date-picker__trigger"
         placeholder="Check In"
         readonly
@@ -20,23 +21,57 @@
       <input
         :id="inputNames.CHECK_OUT"
         :class="{'base-date-picker__trigger--active': activeInput === inputNames.CHECK_OUT}"
+        @click="showCalendar(inputNames.CHECK_OUT)"
         class="base-date-picker__trigger"
         placeholder="Check Out"
         readonly
       />
+
+      <transition name="slide-fade">
+        <keep-alive>
+          <Calendar
+            v-if="isCalendarVisible"
+            :activeInput="activeInput"
+            @hideCalendar="hideCalendar"
+          />
+        </keep-alive>
+      </transition>
     </div>
   </div>
 </template>
 
 <script>
+  import Calendar from "@/components/calendar/Calendar.vue";
+
   import {formInputNames} from "@/enums/formInputNames.js";
 
   export default {
+    components: {
+      Calendar,
+    },
+
     data() {
       return {
         activeInput: null,
         inputNames: formInputNames,
+        isCalendarVisible: false,
       };
+    },
+
+    methods: {
+      clearActiveInput() {
+        this.activeInput = null;
+      },
+
+      hideCalendar() {
+        this.isCalendarVisible = false;
+        this.activeInput = null;
+      },
+
+      showCalendar(activeInput) {
+        this.isCalendarVisible = true;
+        this.activeInput = activeInput;
+      },
     },
   };
 </script>
---
+++ src/components/calendar/Calendar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div v-click-outside="hideCalendar" id="calendar" class="calendar">
+    <img
+      class="calendar__triangle"
+      :class="{
+        'calendar__triangle--right': activeInput === inputNames.CHECK_OUT,
+      }"
+      :src="require('../../assets/triangle.svg')"
+      alt="triangle-icon"
+    />
+
+    calendar content
+  </div>
+</template>
+
+<script>
+  import ClickOutside from "vue-click-outside";
+
+  import {formInputNames} from "@/enums/formInputNames.js";
+
+  export default {
+    props: {
+      activeInput: {
+        type: String,
+      },
+    },
+
+    directives: {
+      ClickOutside,
+    },
+
+    computed: {
+      inputNames() {
+        return formInputNames;
+      },
+    },
+
+    methods: {
+      hideCalendar(event) {
+        if (
+          event.target.id === formInputNames.CHECK_IN ||
+          event.target.id === formInputNames.CHECK_OUT
+        ) {
+          return;
+        }
+
+        this.$emit("hideCalendar");
+      },
+    },
+  };
+</script>
